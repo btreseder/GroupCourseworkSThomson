0f4475513c8d4cf3f7c22c83993d48e930602e18
Tomas <tc751@exeter.ac.uk>
2025-03-18 13:14:40 +0000

    Added SHC

--- constants.m
+++ constants.m
@@ -1,12 +1,15 @@
 clear
 close all
 
+% This generates "workspace_consts.mat" file, the constants that can be
+% used anywhere.
 
-z = 0:1:20000;
-g = 9.81;
-tau_0 = 2.2;
+z = 0:1:20000;  % Range of z values evaluated (0km to 20km in metres)
+g = 9.81;       % Acceleration due to gravity
+tau_0 = 2.2;    
 H_a = 2000;
 tau = tau_0*exp(-z./H_a);
 S_0 = 238;
 sigma = 5.67*10^(-8);
-R=287.04;   %Specific Gas Constant [Jkg-1K-1]
+R=287.04;       %Specific Gas Constant [Jkg-1K-1]
+p_z0 = 10^5;    %Pressure at z = 0 is 1x10^5 [Pa]
