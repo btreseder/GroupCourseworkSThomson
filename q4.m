75711cde625459b4c4f90d705f3ae21e8ae8faa0
B Treseder <146537907+mrboocodes@users.noreply.github.com>
2025-03-18 13:20:53 +0000

    fix q4_findP
    change everything

--- q4.m
+++ q4.m
@@ -5,9 +5,9 @@ g=9.81;     %Gravity
 R=287.04;   %Specific Gas Constant [Jkg-1K-1]
 T=270;      %Temperature [K] 
 
-zn=zeros(100,1); 
+%zn=zeros(100,1); 
 
-[zn,p] = findp(h,g,R,zn,T);
+[zn,p] = q4_findp(h,g,R,zn,T);
 
 figure
 plot(p,zn)
