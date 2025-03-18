dafee9768fe13ee3c5f32d91ef4d9c0cb9c01f94
Tomas <tc751@exeter.ac.uk>
2025-03-18 12:37:46 +0000

    Added workspace_consts.mat

--- constants.m
+++ constants.m
@@ -1,8 +1,12 @@
-z = 0:1:200000;
+clear
+close all
+
+
+z = 0:1:20000;
 g = 9.81;
 tau_0 = 2.2;
-tau = tau_0*exp(-z./H_a);
 H_a = 2000;
+tau = tau_0*exp(-z./H_a);
 S_0 = 238;
 sigma = 5.67*10^(-8);
 R=287.04;   %Specific Gas Constant [Jkg-1K-1]
