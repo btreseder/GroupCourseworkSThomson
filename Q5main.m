043933fe81e6ae9b2469e32f069ebb13ba58ffc9
B Treseder <146537907+mrboocodes@users.noreply.github.com>
2025-03-25 13:20:41 +0000

    q5 figure

--- Q5main.m
+++ Q5main.m
@@ -2,8 +2,8 @@
 
 load("workspace_consts.mat")
 
-tn = 6000000; % 1 week in seconds
-dt = 10000;
+tn = 604800*10; % 10 week in seconds
+dt = 9600;
 
 U_0 = S_0;  D_0 = 0;
 Cp = 7*R/2; %Specific Heat Capacity
@@ -18,7 +18,7 @@ p = zeros(length(z));
 %p(1,:) = p_z0;
 
 
-figure; hold on;
+%figure; hold on;
 for n= 1:(tn/dt)
     p(n,:) = q4_findp( p_z0 , g , R , T(n,:) );
     
@@ -46,9 +46,10 @@ t = 1:dt:tn+1;
 %%
 figure;
 hold on
-for i = 1:50:length(t)
+for i = 1:63:length(t)
     plot(T(i,1:20000),z(1:20000))
 end
-
-
+xlabel('Temperature (K)')
+ylabel('Height (z)')
+legend('Intial Condition','1 week','2 weeks', '3 weeks', '4 weeks','5 weeks','6 weeks','7 weeks','8 weeks', '9 weeks', '10 weeks')
 %%
