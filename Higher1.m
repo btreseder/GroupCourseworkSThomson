9efd5858259eefafc2a1dac2371eab19aa09d2d4
FinbarJW <149806837+FinbarJW@users.noreply.github.com>
2025-03-26 15:38:35 +0000

    tidy

--- Higher1.m
+++ Higher1.m
@@ -28,34 +28,41 @@ T2 = (B1./sigma).^(1/4);
 p1 = zeros(length(z));
 p2 = zeros(length(z));
 
-
-for n= 1:(tn/dt)
+% Forward Euler for 360ppmv
+for n= 1:(tn/dt)           % Horizontal Position in Array
+    j = length(T1(n,:))-1; % Vertical Position in Array
+   
+    % Pressure Array position
     p1(n,:) = find_p( p_z0 , g , R , T1(n,:) );
     
+    % Plank Function Numerical solution
     B1 = sigma.*T1(n,:).^4;
 
-    Unum1 = find_U(B1,tau,U_0);
-    Dnum1 = find_D(B1,tau,D_0);
+    Unum1 = find_U(B1,tau,U_0); % Upward Flux Numerical Solution
+    Dnum1 = find_D(B1,tau,D_0); % Downward Flux Numerical Solution
 
+    % Net Flux differential
     dNdp1 = finddNdp( Unum1, Dnum1, p1(n,:) );
-    
-    j = length(T1(n,:))-1;
 
     T1(n+1,1:j) = T1(n,1:j) + dt *(g/Cp)* dNdp1;
    
 end
 
-for m = 1:(tn/dt)
+% Forward Euler for 720ppmv
+for m = 1:(tn/dt)          % Horizontal Position in Array
+    k = length(T2(m,:))-1; % Vertical Position in Array
+    
+    % Pressure Array position
     p2(m,:) = find_p(p_z0 , g , R , T2(m,:));
     
+    % Plank Function Numerical solution
     B2 = sigma.*T2(m,:).^4;
 
-    Unum2 = find_U(B2,tau2,U_0);
-    Dnum2 = find_D(B2,tau2,D_0);
-
+    Unum2 = find_U(B2,tau2,U_0); % Upward Flux Numerical solution
+    Dnum2 = find_D(B2,tau2,D_0); % Downward Flux Numerical solution
+    
+    % Net Flux differential
     dNdp2 = finddNdp( Unum2, Dnum2, p2(m,:));
-
-    k = length(T2(m,:))-1;
 
     T2(m+1,1:k) = T2(m,1:k) + dt *(g/Cp)* dNdp2;
 end
@@ -64,16 +71,16 @@ t = 1:dt:tn+1;
 
 % Plot for C02 = 360ppmv
 i = 1:50:length(t);
-    plot(T1(i,1:20000),z(1:20000), T1(length(t),1:1000:20000),z(1:1000:20000), '--')
-    title("C02=360ppmv")
+    plot(T1(i,1:20000),z(1:20000), ...
+         T1(length(t),1:1000:20000),z(1:1000:20000), '--')
     xlabel('Temperature (K)')
     ylabel('Height (m)')
 
 % Plot for C02 = 720ppmv
 figure;
 y = 1:length(t);
-    plot(T2(y,1:20000),z(1:20000), T2(length(t),1:1000:20000),z(1:1000:20000), '--')
-    title("C02=720ppmv")
+    plot(T2(y,1:20000),z(1:20000), ...
+         T2(length(t),1:1000:20000),z(1:1000:20000), '--')
     xlabel('Temperature (K)')
     ylabel('Height (m)')
     
