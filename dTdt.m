d3e6316ad180f6c8e4b4f83b640929123e5e1982
B Treseder <146537907+mrboocodes@users.noreply.github.com>
2025-03-26 15:42:45 +0000

    Final

--- dTdt.m
+++ dTdt.m
@@ -2,7 +2,9 @@ function dTdt = dTdt(g,C_p,dNdp,kappa,R,T,p,theta_n,a,dtheta)
     dTdt = zeros(1,length(T));
     for i = 2:length(T)-1
 
+        %finite difference wrt theta
         dTdtheta = (T(i)-T(i+1))/dtheta;
+        %second order finite difference wrt theta
         dT2dtheta2 = (T(i+1) - 2*T(i) + T(i-1))/(dtheta^2);
 
         d_cosd = cos(theta_n)*dT2dtheta2 - sin(theta_n)*dTdtheta;
