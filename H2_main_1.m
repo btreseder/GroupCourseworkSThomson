b8ec794ddb1236f967885f3151e4971ce8874dc4
Tomas <tc751@exeter.ac.uk>
2025-03-26 15:09:05 +0000

    higher 2 update

--- H2_main_1.m
+++ H2_main_1.m
@@ -2,8 +2,6 @@ clear;
 close all;
 
 % Change start point to be the radius of equilibrium (Q6)
-
-
 load("10week_higher2.mat")
 
 for timestep = [2, 64, 631]
@@ -24,5 +22,47 @@ for timestep = [2, 64, 631]
     hold off;
 end
 
+
+
+%%
+clear;
+load("10week_higher2_q6.mat")
+
+for timestep = [2, 64, 631]
+    figure;     
+    hold on;
+
+    contour(   theta_deg(1:length(theta)), ...
+                z(1:length(z)-1), ...
+                squeeze(T_new(timestep,1:length(z)-1, 1:length(theta))), ...
+                "ShowText",true, ...
+                "LabelFormat","%1.f K", ...
+                "LabelSpacing",216, ...
+                "LabelColor","black")
+
+    xlabel("Latitude (degrees)")
+    ylabel("Altitude (metres)")
+
+    hold off;
+end
+
+
+%%
+clear; figure; hold on;
+
+load("10week_q6.mat")
+
+contourf(   theta_deg(1:length(theta)), ...
+            z(1:length(z)-2), ...
+            squeeze(T(631,1:length(z)-2, 1:length(theta))), ...
+            "ShowText",true, ...
+            "LabelFormat","%1.f K", ...
+            "LabelSpacing",216, ...
+            "LabelColor","white")
+
+xlabel("Latitude (degrees)")
+ylabel("Altitude (metres)")
+
+hold off;
 
 
--- dTdt.m
+++ dTdt.m
@@ -1,15 +1,19 @@
 function dTdt = dTdt(g,C_p,dNdp,kappa,R,T,p,theta_n,a,dtheta)
     dTdt = zeros(1,length(T));
-    for i = 1:length(T)-1
+    for i = 2:length(T)-1
         
-        dTdtheta = (T(i)-T(i+1))/dtheta;
+        %dTdtheta2 = (cos(theta_n)*dTdtheta)/dtheta;
+        %dTdtheta2 = (-sin(theta_n)*dTdtheta);
 
-        dTdtheta2 = (cos(theta_n)*dTdtheta)/dtheta;
+        dTdtheta = (T(i)-T(i+1))/dtheta;
+        dT2dtheta2 = (T(i+1) - 2*T(i) + T(i-1))/(dtheta^2);
+
+        d_cosd = cos(theta_n)*dT2dtheta2 - sin(theta_n)*dTdtheta;
 
         % PRE-UPDATE
         %dTdt(i) = (g/C_p)*dNdp(i) + (kappa*R*T(i))/(p(i)*C_p*a^2*cos(theta_n))*dTdtheta2;
 
-        dTdt(i) = (g/C_p)*dNdp(i) + (kappa)/(p(i)*a^2*cos(theta_n))*dTdtheta2;
+        dTdt(i) = (g/C_p)*dNdp(i) + (kappa)/(p(i)*a^2*cos(theta_n))*d_cosd;
 
     end
 end
