4df7b0a2ed63b195ea8a2614fd4186ffef4afe93
FinbarJW <149806837+FinbarJW@users.noreply.github.com>
2025-03-26 15:07:46 +0000

    tidy

--- Q6main.m
+++ Q6main.m
@@ -6,46 +6,48 @@ week_sec = 604800; % 1 week in seconds
 tn = 6*week_sec;   % 6 weeks in seconds  
 dt = 9600;         % Time-step (240 minutes)
 
+% ICs for theta
+theta_min = - pi/2;                 % Starting theta
+theta_max = pi/2;                   % End theta
+dtheta = pi/32;                     % delta theta
+theta = theta_min:dtheta:theta_max; % delta theta array
 
-Cp = 7*R/2; %Specific Heat Capacity
+T = zeros(tn/dt,length(z), length(theta)); % Zeros Array for Temperature
+p = zeros(length(z));                      % Zeros Array for Pressure
+
+% Planks Function Analytical Solution
+B1 = B(tau,S_0);
+
+%ICs and Constants
+T(1,:) = 175; % Initial Temperature
+Cp = 7*R/2;   % Specific Heat Capacity
+D_0 = 0;      % Initial Downward Flux value
 
 % Initial condition for find_U
 S = @(theta, S_0, delta) S_0*(1+0.25*delta *(1-3*(sin(theta))^2));
 
-
-% Initial condition for find_D
-D_0 = 0;
-
-theta_min = - pi/2;
-theta_max = pi/2;
-dtheta = pi/32;
-theta = theta_min:dtheta:theta_max;
-
-T = zeros(tn/dt,length(z), length(theta));
-T(1,:) = 175;
-B1 = B(tau,S_0);
-
-p = zeros(length(z));
-%p(1,:) = p_z0;
-
-
 for i = 1:length(theta)
-    for n= 1:(tn/dt)
+    for n= 1:(tn/dt)            % Horizontal Position in Array
+        j = length(T(n,:,i))-1; % Vertical Position in Array
+        
+         % Pressure Array position
         p(n,:) = find_p( p_z0 , g , R , T(n,:,i) );
         
         B1 = sigma.*T(n,:,i).^4;
-        
+        % Upward Flux Numerical solution
         Unum = find_U(B1,tau,S(theta(i), S_0, delta));
+
+        %Downward Flux Numerical solution
         Dnum = find_D(B1,tau,D_0);
-    
+
+        % Net Flux Numerical solutions
         dNdp = finddNdp( Unum, Dnum, p(n,:) );
-        
-        j = length(T(n,:,i))-1;
     
         T(n+1,1:j,i) = T(n,1:j,i) + dt *(g/Cp)* dNdp;
             
     end
 end
+
 %%
 t = 1:dt:tn+1;
 
